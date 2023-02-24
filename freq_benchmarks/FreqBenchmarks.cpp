09146fa0835a86585fdde6de3360c415d5c392f9
olefirenque <egor.olefirenko892@gmail.com>
2023-02-24 15:52:47 +0300

    feat(tests): googleTests target, implement logical tests, make more common benchmarks declaration

--- freq_benchmarks/FreqBenchmarks.cpp
+++ freq_benchmarks/FreqBenchmarks.cpp
@@ -4,6 +4,17 @@
 #include <benchmark/benchmark.h>
 
 #include "../src/freq.h"
+#include "../src/dummy/freq_dummy.h"
+
+#define BASE_FREQ_BENCHMARK(TARGET) \
+    BENCHMARK(TARGET) \
+    ->MeasureProcessCPUTime() \
+    ->UseRealTime() \
+    ->Iterations(ITERATIONS) \
+    ->DenseRange(0, 4, 1) \
+    ->Unit(benchmark::kSecond);
+
+constexpr size_t ITERATIONS = 3;
 
 constexpr std::array<std::string_view, 5> files{
     "test-1000.txt",
@@ -13,18 +24,34 @@ constexpr std::array<std::string_view, 5> files{
     "test-10000000.txt",
 };
 
-static void BM_CountFreqOrdinaryFile(benchmark::State &state) {
+template<typename F>
+static void run(benchmark::State &state, F f, const std::string &test_dir) {
     for (auto _ : state) {
         auto file = std::string(files[state.range(0)]);
         state.SetLabel(file);
-        const auto &data = process_file("../freq_benchmarks/test_cases/" + file);
+        const auto &data = f(test_dir + file);
     }
 }
 
-BENCHMARK(BM_CountFreqOrdinaryFile)
-    ->MeasureProcessCPUTime()
-    ->UseRealTime()
-    ->Iterations(10)
-    ->DenseRange(0, 4, 1)
-    ->Unit(benchmark::kSecond);
+static void BM_CountDummyFreqDictWords(benchmark::State &state) {
+    run(state, process_file_dummy, "../test_cases/dict_words/");
+}
+
+static void BM_CountDummyFreqSingleWord(benchmark::State &state) {
+    run(state, process_file_dummy, "../test_cases/dict_words/");
+}
+
+static void BM_CountFreqDictWords(benchmark::State &state) {
+    run(state, process_file, "../test_cases/dict_words/");
+}
+
+static void BM_CountFreqSingleWord(benchmark::State &state) {
+    run(state, process_file, "../test_cases/single_word/");
+}
+
+BASE_FREQ_BENCHMARK(BM_CountDummyFreqDictWords);
+BASE_FREQ_BENCHMARK(BM_CountDummyFreqSingleWord);
+BASE_FREQ_BENCHMARK(BM_CountFreqDictWords);
+BASE_FREQ_BENCHMARK(BM_CountFreqSingleWord);
+
 BENCHMARK_MAIN();
---
+++ freq_tests/FreqTests.cpp
@@ -0,0 +1,35 @@
+#include "gtest/gtest.h"
+
+#include "../src/freq.h"
+#include "../src/dummy/freq_dummy.h"
+
+void base_test(const std::string &test_dir) {
+    for (const std::string &test : {
+        "test-200.txt",
+        "test-1000.txt",
+        "test-10000.txt",
+        "test-100000.txt",
+        "test-1000000.txt",
+        "test-10000000.txt",
+    }) {
+        const std::string filename(test_dir + test);
+        auto actual = process_file(filename);
+        auto x = process_file_dummy(filename);
+        auto expected = FreqMap(x.begin(), x.end());
+
+        EXPECT_EQ(actual, expected);
+    }
+}
+
+TEST(freq_test, dict_words_test) {
+    base_test("../test_cases/dict_words/");
+}
+
+TEST(freq_test, single_word_test) {
+    base_test("../test_cases/single_word/");
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
