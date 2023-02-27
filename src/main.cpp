92fef040241d13d03aa863e43e986980c94ce0c4
olefirenque <egor.olefirenko892@gmail.com>
2023-02-27 14:57:28 +0300

    feat: run processing from main

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
+#include <fstream>
 #include "freq.h"
 
-int main() {
-    const auto filename = "../test.txt";
-    const auto &data = read_data(filename);
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        std::cerr << "Usage: " << argv[0] << " [input_file] [output_file]>" << std::endl;
+        std::exit(1);
+    }
 
-    std::cout << data.size() << '\n';
+    const char *input_file = argv[1];
+    const char *output_file = argv[2];
+
+    // TODO: apply sort by freq and lexicographically
+    const FreqMap &data = process_file(input_file);
+
+    std::ofstream output;
+    output.open(output_file);
+    for (const auto &[word, count] : data) {
+        output << word << ' ' << count << '\n';
+    }
+    output.close();
 
     return 0;
 }
