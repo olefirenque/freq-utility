d2bb9ae959a312b7b82211f53e0804c427fbf1dc
olefirenque <egor.olefirenko892@gmail.com>
2023-03-02 20:23:37 +0300

    feat: add sorting by freq and lexicographically

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,17 @@ int main(int argc, char *argv[]) {
     const char *input_file = argv[1];
     const char *output_file = argv[2];
 
-    // TODO: apply sort by freq and lexicographically
-    const FreqMap &data = process_file_blocking_read(input_file);
+    const auto &data = process_file_blocking_read(input_file);
+
+    std::vector<std::pair<std::string, size_t>> word_freq_pairs(
+        std::move_iterator(data.begin()),
+        std::move_iterator(data.end())
+    );
+    std::sort(word_freq_pairs.begin(), word_freq_pairs.end(), [](const auto &p1, const auto &p2) {
+      auto &[word1, freq1] = p1;
+      auto &[word2, freq2] = p2;
+      return std::tie(freq2, word1) < std::tie(freq1, word2);
+    });
 
     std::ofstream output;
     output.open(output_file);
